4835b93ae329e498d2674bde72cb79225ae8fadb
yuldashevdeveloper <yuldashevj700@gmail.com>
2025-06-10 01:18:25 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const popupEl = document.querySelector(".popup");
+
+function togglePopup(){
+    popupEl.classList.toggle("active")
+}
+
+
+const sidebarEl = document.querySelector(".sidebar")
+
+function sidebarFc(){
+    sidebarEl.classList.toggle("activee")
+}
